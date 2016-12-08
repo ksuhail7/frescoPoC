f56a13ed6ed6012310be41af084874a4e0361a6e
suhail kandanur <ksuhail7@gmail.com>
2016-12-08 10:56:48 -0500

    document service

---
+++ src/main/java/com/suhailkandanur/fresco/service/DocumentService.java
@@ -0,0 +1,33 @@
+package com.suhailkandanur.fresco.service;
+
+import com.suhailkandanur.fresco.entity.Document;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by suhail on 2016-12-08.
+ */
+public interface DocumentService {
+    Document getDocument(String storeId, String docId);
+
+    List<Document> getDocumentsInStore(String storeId);
+
+    File getDocumentFile(String storeId, String docId);
+
+    default File getDocumentFile(Document document) {
+        if (document != null)
+            return getDocumentFile(document.getStoreId(), document.getDocumentId());
+        return null;
+    }
+
+    File getDocumentFile(String storeId, String docId, long version);
+
+    default File getDocumentFile(Document document, long version) {
+        if(document != null)
+            return getDocumentFile(document.getStoreId(), document.getDocumentId(), version);
+        return null;
+    }
+
+
+}
