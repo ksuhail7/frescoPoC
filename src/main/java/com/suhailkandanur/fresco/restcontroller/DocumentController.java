165a56cb671fd7b33c66aaca5dc5300a088eae84
suhail kandanur <ksuhail7@gmail.com>
2016-12-07 21:52:22 -0500

    repository and store creation

--- src/main/java/com/suhailkandanur/fresco/restcontroller/DocumentController.java
+++ src/main/java/com/suhailkandanur/fresco/restcontroller/DocumentController.java
@@ -1,10 +1,16 @@
 package com.suhailkandanur.fresco.restcontroller;
 
 import com.suhailkandanur.fresco.entity.Document;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import javax.naming.NoInitialContextException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -15,13 +21,41 @@ import java.util.Map;
 @CrossOrigin
 public class DocumentController {
 
-    @GetMapping("/document/{id}")
-    public Document getDocument(@PathVariable String docId) {
+    private static final Logger logger = LoggerFactory.getLogger(DocumentController.class);
+
+
+
+    @GetMapping("/document/{storeId}/{docId}")
+    public Document getDocument(@PathVariable String storeId, @PathVariable String docId) {
         throw new NotImplementedException();
     }
 
     @PostMapping("/document")
     public Document createDocument(@RequestBody Map<String, String> request) {
         throw new NotImplementedException();
+    }
+
+    @GetMapping("/document/{storeId}")
+    public List<Document> getDocumentsInStore(@PathVariable String storeId) {
+        throw new NotImplementedException();
+    }
+
+    @GetMapping("/document/{storeId}/{docId}")
+    public void retrieveFile(@PathVariable String storeId, @PathVariable String docId, HttpServletResponse response) {
+        InputStream inputStream = null;
+        try {
+            IOUtils.copy(inputStream, response.getOutputStream());
+            response.flushBuffer();
+        } catch(IOException e) {
+            logger.error("unable to stream file, exception: {}", e.getMessage());
+        } finally {
+            if(inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch(IOException ioe) {
+                    logger.error("unable to close input stream, exception: {}", ioe.getMessage());
+                }
+            }
+        }
     }
 }
