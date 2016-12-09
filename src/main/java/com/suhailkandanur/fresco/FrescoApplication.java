d4eff1cfe2c8d6dc6ad6563a7ed4f29a05143cb8
suhail kandanur <ksuhail7@live.com>
2016-12-09 16:06:48 -0500

    add logging

--- src/main/java/com/suhailkandanur/fresco/FrescoApplication.java
+++ src/main/java/com/suhailkandanur/fresco/FrescoApplication.java
@@ -1,5 +1,7 @@
 package com.suhailkandanur.fresco;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -7,6 +9,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableMongoRepositories
 public class FrescoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(FrescoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(FrescoApplication.class, args);
 	}
