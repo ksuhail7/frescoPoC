a2afe92d776977a6b4668824d0c5462e84712948
suhail kandanur <ksuhail7@gmail.com>
2016-12-11 10:14:42 -0500

    store component and service

--- frescoportal/src/app/store/store.component.ts
+++ frescoportal/src/app/store/store.component.ts
@@ -8,13 +8,24 @@ import {StoreService} from "./store.service";
 })
 
 export class StoreComponent implements OnInit {
-  constructor(private storeService: StoreService) {}
+  constructor(private storeService: StoreService) {
+  }
 
   stores: Store[];
 
   model = new Store("", "", -1);
 
   ngOnInit(): void {
+    this.getAllStores();
+  }
+
+  getAllStores(): void {
+    this.storeService
+      .getAllStores()
+      .then(stores => {
+        console.debug("store list ", stores);
+        this.stores = stores;
+      });
   }
 
 }
--- frescoportal/src/app/store/store.service.ts
+++ frescoportal/src/app/store/store.service.ts
@@ -1,7 +1,34 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import {Http, Headers} from "@angular/http";
+import {Store} from "./store";
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class StoreService {
-    constructor(http: Http) {}
+  constructor(private http: Http) {
+  }
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  private frescoUrl = 'http://localhost:9092';
+
+  url = `${this.frescoUrl}/stores`;
+
+  getAllStores(): Promise<Store[]> {
+    return this.http
+      .get(this.url)
+      .toPromise()
+      .then(response => {
+        console.debug("response ", response);
+        return response.json() as Store[];
+      }).catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
+
+
 }
